5a085e88f86b9d307c5ffde45538591d9a5e9742
Eduardo <eduardofreitasfr@gmail.com>
2024-11-08 15:15:04 -0300

    Adicionado controlador de clientes

---
+++ src/main/java/com/bank/banco/virtual/controller/ClienteController.java
@@ -0,0 +1,21 @@
+package com.bank.banco.virtual.controller;
+
+import com.bank.banco.virtual.model.ClienteModel;
+import com.bank.banco.virtual.service.ClienteService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/user/controlador")
+public class ClienteController {
+    private ClienteService service;
+
+    public ResponseEntity<HttpStatus> registerUser(ClienteModel cliente) {
+        if (service.createrUser(cliente)) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        
+    }
+}

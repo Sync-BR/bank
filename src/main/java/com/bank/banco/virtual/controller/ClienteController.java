d2e6919abc5a183c6945042f636a67bdb8cf3ac0
Eduardo <eduardofreitasfr@gmail.com>
2024-11-08 15:22:05 -0300

    Adicionado anotação para efetuar uma requisição post

--- src/main/java/com/bank/banco/virtual/controller/ClienteController.java
+++ src/main/java/com/bank/banco/virtual/controller/ClienteController.java
@@ -5,17 +5,18 @@ import com.bank.banco.virtual.service.ClienteService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/user/controlador")
 public class ClienteController {
     private ClienteService service;
-
+    @PostMapping("/creater")
     public ResponseEntity<HttpStatus> registerUser(ClienteModel cliente) {
         if (service.createrUser(cliente)) {
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        
+
     }
 }

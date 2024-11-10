4065ee32801c2b4a1ed51e1236593ea539dd836f
Eduardo <eduardofreitasfr@gmail.com>
2024-11-10 20:14:02 -0300

    Adicionado log para acompanhar os testes

--- src/main/java/com/bank/banco/virtual/service/ClienteService.java
+++ src/main/java/com/bank/banco/virtual/service/ClienteService.java
@@ -2,35 +2,46 @@ package com.bank.banco.virtual.service;
 
 import com.bank.banco.virtual.model.ClienteModel;
 import com.bank.banco.virtual.repository.ClienteRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@Service
 public class ClienteService {
+    private static final Logger log = LogManager.getLogger(ClienteService.class);
     @Autowired
-    private ClienteRepository clienteRepository;
+    private  ClienteRepository clienteRepository;
 
     public Boolean createrUser(ClienteModel clienteModel) {
-        ClienteModel cliente = clienteRepository.save(clienteModel);
-        if (cliente != null) {
+        if (clienteModel != null) {
+            log.info("Cliente adicionado com sucesso");
+            clienteRepository.save(clienteModel);
             return true;
         }
+        log.error("Erro ao cadastrar cliente");
         return false;
     }
 
-    public Boolean checkCpf(String cpf){
+    public Boolean checkCpf(String cpf) {
         ClienteModel cliente = clienteRepository.findByCpf(cpf);
         if (cliente != null) {
+            log.info("Cpf encontrado no banco de dados");
             return true;
         }
+        log.info("CPF: " +cpf+ " não encontrado no banco de dados!");
         return false;
     }
-    public Boolean checkEmail(String email){
+
+    public Boolean checkEmail(String email) {
         ClienteModel cliente = clienteRepository.findByEmail(email);
         if (cliente != null) {
+            log.info("Email encontrado no banco de dados");
             return true;
         }
+        log.info("Email " +email+ " não foi encontrado no banco de dados");
         return false;
     }
 }

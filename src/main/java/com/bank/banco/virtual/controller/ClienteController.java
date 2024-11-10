62a26697ecae385aec097a4c8f02c461ebd4aef6
Eduardo <eduardofreitasfr@gmail.com>
2024-11-10 20:07:07 -0300

    Adicionado api para upload de arquivo

--- src/main/java/com/bank/banco/virtual/controller/ClienteController.java
+++ src/main/java/com/bank/banco/virtual/controller/ClienteController.java
@@ -1,29 +1,42 @@
 package com.bank.banco.virtual.controller;
 
 import com.bank.banco.virtual.model.ClienteModel;
+import com.bank.banco.virtual.repository.ClienteRepository;
 import com.bank.banco.virtual.service.ClienteService;
 
 import com.bank.banco.virtual.service.FileUploadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@RestController
 @RequestMapping("/api/user/controlador")
 public class ClienteController {
+    private static final Logger log = LoggerFactory.getLogger(ClienteController.class);
+    @Autowired
     private ClienteService service;
 
-    @PostMapping("/creater")
-    public ResponseEntity<?> registerUser(@RequestBody ClienteModel cliente, @RequestParam("file") MultipartFile file) {
+    @PostMapping("/uploadarchive")
+    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
         FileUploadService uploadService = new FileUploadService();
         if (uploadService.uploadImage(file)) {
-            if (service.createrUser(cliente)) {
-                return new ResponseEntity<>(HttpStatus.CREATED);
-            }
-            return ResponseEntity.status(HttpStatus.OK).body("Falha no upload da imagem.");
-
+            return ResponseEntity.status(HttpStatus.OK).body("Sucesso ao enviar imagem");
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Falha no upload da imagem.");
+    }
+    @PostMapping("/creater")
+    public ResponseEntity<?> registerUser(@RequestBody ClienteModel cliente) {
+        if (service.createrUser(cliente)) {
+            log.info("Cliente cadastrado com sucesso");
+            return ResponseEntity.status(HttpStatus.CREATED).body("Cliente cadastrado com sucesso");
+        }
+//        }
+        log.error("Erro ao cadastrar cliente");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro ao cadastrar cliente");
 
     }
 

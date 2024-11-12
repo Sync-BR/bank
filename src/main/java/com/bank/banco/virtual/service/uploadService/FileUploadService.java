a207bc16a733011aa9255225a998d8e9ce332af6
Eduardo <eduardofreitasfr@gmail.com>
2024-11-12 07:44:12 -0300

    Refatorado

---
+++ src/main/java/com/bank/banco/virtual/service/uploadService/FileUploadService.java
@@ -0,0 +1,25 @@
+package com.bank.banco.virtual.service.uploadService;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUploadService {
+    private static final Log log = LogFactory.getLog(FileUploadService.class);
+    private final String dir = "E:/Java/Projetos SpringBoot/banco-virtual/src/main/resources/static/src/img/perfil/";
+
+    public boolean uploadImage(MultipartFile file) {
+        try {
+            File destinationFile = new File(dir + file.getOriginalFilename());
+            file.transferTo(destinationFile);
+            log.info("Imagem enviada com sucesso!");
+            return true;
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+}
